db92f3887f833c914e309c20d8a4bc492472a9de
ryanbrown <r_yan@outlook.com>
2016-03-08 16:28:25 -0700

    Playing around with json

--- js/script.js
+++ js/script.js
@@ -20,3 +20,28 @@ function viewAboutPage() {
   aboutPage.style.left = 0;
   currentPage = aboutPage;
 }
+function returnDrugNames(){
+
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://dailymed.nlm.nih.gov/dailymed/services/drugnames.json";
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var myArr = JSON.parse(xmlhttp.responseText);
+            myFunction(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    function myFunction(arr) {
+        var out = "";
+        var i;
+        for(i = 0; i < arr.length; i++) {
+            out += 'page';
+        }
+        document.getElementById("log").innerHTML = out;
+    }
+
+
+}
