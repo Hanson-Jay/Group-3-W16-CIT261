3415836811a62de3f4907d88a87dbd934862bbd5
sarahbroat <sarahbroat@192.168.1.10>
2016-03-12 18:16:32 -0500

    3/12/2016 - Added slider, updated logo, trying to get slider to transition colors when moved to final position (still working on that)

--- js/script.js
+++ js/script.js
@@ -64,3 +64,31 @@ function store() {
   document.getElementById("1").innerHTML = out;
   document.getElementById("2").innerHTML = out;
 }
+
+/*Medication Log Page Sliders */
+window.addEventListener('load', function(){
+
+                var slider = document.getElementById('slider'),
+                boxleft, // left position of moving box
+                startx, // starting x coordinate of touch point
+                dist = 0, // distance traveled by touch point
+                touchobj = null // Touch object holder
+
+                slider.addEventListener('touchstart', function(e){
+                    touchobj = e.changedTouches[0] // reference first touch point
+                    boxleft = parseInt(slider.style.left) // get left position of box
+                    startx = parseInt(touchobj.clientX) // get x coord of touch point
+                    e.preventDefault() // prevent default click behavior
+                }, false)
+
+                slider.addEventListener('touchmove', function(e){
+                    touchobj = e.changedTouches[0] // reference first touch point for this event
+                    var dist = parseInt(touchobj.clientX) - startx // calculate dist traveled by touch point
+                    // move box according to starting pos plus dist
+                    // with lower limit 0 and upper limit 350 so it doesn't move outside box:
+                    slider.style.left = ( (boxleft + dist > 21)? 21 : (boxleft + dist < 0)? 0 : boxleft + dist ) + 'px'
+                    e.preventDefault()
+                    slider.classList.add('active')
+                }, false)
+
+                }, false)
